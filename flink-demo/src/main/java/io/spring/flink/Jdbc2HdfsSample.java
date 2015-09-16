7432bf70b0982d995e0b4f98a81605331d6c8b5d
Gunnar Hillert <ghillert@pivotal.io>
2015-09-16 09:17:12 -0400

    Add DataSet transformation

--- flink-demo/src/main/java/io/spring/flink/Jdbc2HdfsSample.java
+++ flink-demo/src/main/java/io/spring/flink/Jdbc2HdfsSample.java
@@ -18,6 +18,9 @@ package io.spring.flink;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.GroupReduceFunction;
+import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
@@ -32,10 +35,12 @@ import org.apache.flink.api.java.tuple.Tuple7;
 import org.apache.flink.api.java.typeutils.TupleTypeInfo;
 import org.apache.flink.core.fs.FileSystem.WriteMode;
 import org.apache.flink.core.fs.Path;
+import org.apache.flink.util.Collector;
 
 /**
  * @author Gunnar Hillert
  */
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class Jdbc2HdfsSample {
 
 		public static void main(String[] args) throws Exception {
@@ -52,7 +57,6 @@ public class Jdbc2HdfsSample {
 			// set up the execution environment
 			final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-			@SuppressWarnings("unchecked")
 			final DataSet<Tuple14<Long, Long, Integer, Long, String, Integer, Integer,
 				String, Long, String, Integer, Integer, Integer, Long>> dataSetPosts =
 					env.createInput(getInputFormat("select ID, VERSION, POST_TYPE, ACCEPTED_ANSWER_ID,"
@@ -69,7 +73,7 @@ public class Jdbc2HdfsSample {
 
 			dataSetPosts.output(getOutFormat("posts.csv", writeToLocalFs));
 
-			@SuppressWarnings("unchecked")
+
 			final DataSet<Tuple7<Long, Long, Long, String, String, Long, Integer>> dataSetComments =
 					env.createInput(getInputFormat("select ID, VERSION, POST_ID, VALUE, CREATION_DATE, USER_ID, SCORE from COMMENTS"),
 					new TupleTypeInfo(
@@ -79,21 +83,18 @@ public class Jdbc2HdfsSample {
 
 			dataSetComments.output(getOutFormat("comments.csv", writeToLocalFs));
 
-			@SuppressWarnings("unchecked")
 			final DataSet<Tuple2<Long, Long>> dataSetPostTag = env.createInput(getInputFormat("select POST_ID, TAG_ID from POST_TAG"),
 					new TupleTypeInfo(
 							Tuple2.class, BasicTypeInfo.LONG_TYPE_INFO, BasicTypeInfo.LONG_TYPE_INFO));
 
 			dataSetPostTag.output(getOutFormat("post-tag.csv", writeToLocalFs));
 
-			@SuppressWarnings("unchecked")
 			final DataSet<Tuple3<Long, Long, String>> dataSetTag = env.createInput(getInputFormat("select ID, VERSION, TAG from TAG"),
 					new TupleTypeInfo(
 							Tuple3.class, BasicTypeInfo.LONG_TYPE_INFO, BasicTypeInfo.LONG_TYPE_INFO, BasicTypeInfo.STRING_TYPE_INFO));
 
 			dataSetTag.output(getOutFormat("tag.csv", writeToLocalFs));
 
-			@SuppressWarnings("unchecked")
 			final DataSet<Tuple11<Long, Long, Integer, String, String, String, String, String, Integer, Integer, Integer>> dataSetUsers = env.createInput(getInputFormat("select ID, VERSION, REPUTATION, CREATION_DATE, DISPLAY_NAME, LAST_ACCESS_DATE, LOCATION, ABOUT, VIEWS, UP_VOTES, DOWN_VOTES from USERS"),
 					new TupleTypeInfo(
 							Tuple11.class,
@@ -106,19 +107,36 @@ public class Jdbc2HdfsSample {
 
 			dataSetUsers.output(getOutFormat("users.csv", writeToLocalFs));
 
-			@SuppressWarnings("unchecked")
 			final DataSet<Tuple5<Long, Long, Long, Integer, String>> dataSetVotes = env.createInput(getInputFormat("select ID, VERSION, POST_ID, VOTE_TYPE, CREATION_DATE from VOTES"),
 					new TupleTypeInfo(
 							Tuple5.class, BasicTypeInfo.LONG_TYPE_INFO, BasicTypeInfo.LONG_TYPE_INFO, BasicTypeInfo.LONG_TYPE_INFO, BasicTypeInfo.INT_TYPE_INFO, BasicTypeInfo.STRING_TYPE_INFO));
 
 			dataSetVotes.output(getOutFormat("votes.csv", writeToLocalFs));
 
+			//DataSet Transformation
+			final DataSet top5Tags = dataSetPostTag.groupBy(1)
+				.reduceGroup(new GroupReduceFunction<Tuple2<Long,Long>, Tuple2<Long,Long>>() {
+
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					public void reduce(Iterable<Tuple2<Long, Long>> values, Collector<Tuple2<Long,Long>> out) throws Exception {
+						for (Tuple2<Long, Long> tuple : values) {
+							out.collect(new Tuple2<Long, Long>(tuple.f1, 1L));
+						}
+					}
+
+				}).groupBy(0).sum(1).sortPartition(1, Order.DESCENDING)
+					.first(5).join(dataSetTag)
+					.where(0).equalTo(0)
+					.projectFirst(1).projectSecond(2);
+
+			top5Tags.output(getOutFormat("top5Tags.csv", writeToLocalFs));
 			JobExecutionResult executionResult = env.execute();
 
 			System.out.println("Finished in " + executionResult.getNetRuntime(TimeUnit.MILLISECONDS) + "ms.");
 		}
 
-		//@SuppressWarnings("rawtypes")
 		private static JDBCInputFormat getInputFormat(String query) {
 			return JDBCInputFormat.buildJDBCInputFormat()
 			.setDBUrl("jdbc:mysql://127.0.0.1:3306/batch_demo")
@@ -128,7 +146,6 @@ public class Jdbc2HdfsSample {
 			.setQuery(query).finish();
 		}
 
-		@SuppressWarnings("rawtypes")
 		private static CsvOutputFormat getOutFormat(String filename, boolean writeToLocalFs) {
 
 			final Path outputPath;
@@ -137,7 +154,7 @@ public class Jdbc2HdfsSample {
 				outputPath = new Path("file:///tmp/flink/" + filename);
 			}
 			else {
-				outputPath = new Path("hdfs://localhost:8020/" + filename);
+				outputPath = new Path("hdfs://localhost:8020/flink/" + filename);
 			}
 			final CsvOutputFormat outputFormat = new CsvOutputFormat(outputPath, "\n", "|");
 
